b9961118c8b8456e8844b82d836e48ba336d7b9b
The Winged Rhino <wingedrhino@users.noreply.github.com>
2020-02-26 04:09:19 +0530

    Added decorators

---
+++ decorators/README.md
@@ -0,0 +1,10 @@
+# Python Decorator Playground
+
+Made a decorator with no arguments (these need 2 levels of nested def keywords)
+to print the name of the function being executed.
+
+Made a decorator with an argument (these need 2 levels of nested def keywords)
+to execute decorated function inside the directory specified by the argument.
+
+Also passing various combintions of arguments to the function that's being
+decorated.
---
+++ decorators/decorators.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import os
+from os.path import expanduser
+
+def execute_in_dir(dirname):
+  def execute_in_dir_decorator(callback):
+    def decorated_callback(*args, **kw):
+      cwd = os.getcwd()
+      os.chdir(dirname)
+      callback(*args, **kw)
+      os.chdir(cwd)
+    return decorated_callback
+  return execute_in_dir_decorator
+
+# say_what tells WHAT method is currently being executed
+def say_what(callback):
+  def decorated_callback(*args, **kw):
+    print(f'Begin executing {callback.__name__}')
+    callback(*args, **kw)
+    print(f'Finished executing {callback.__name__}')
+  return decorated_callback
+
+def curdir():
+  print(f'Current directory: {os.getcwd()} ')
+
+@execute_in_dir(expanduser('~'))
+@say_what
+def do_something(arg1, arg2, kw1, kw2=44):
+  curdir()
+  print(f'arg1={arg1}, arg2={arg2}, kw1={kw1}, kw2={kw2}')
+
+curdir()
+do_something(1, 2, kw1=3)
+curdir()
